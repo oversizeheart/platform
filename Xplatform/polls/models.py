65f3fafa8ad478cef000ff7a8f3dde571030ec95
oversizeheart <ye@malyshev-work.ru>
2023-02-18 00:16:35 +0300

    day_2 of dev first django app
    * race condition with database
    * generic views
    * create some automated tests

--- Xplatform/polls/models.py
+++ Xplatform/polls/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 
 
 class Question(models.Model):
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
@@ -12,10 +13,12 @@ class Question(models.Model):
         return self.question_text
     
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
+    
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
